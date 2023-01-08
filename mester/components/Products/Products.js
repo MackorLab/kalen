8612874b5ee071893cb01890ef8276c8f0d71c42
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-09 00:39:26 +0700

    Update Products.js

--- mester/components/Products/Products.js
+++ mester/components/Products/Products.js
@@ -20,12 +20,24 @@ class Products {
         let htmlCatalog = '';
 
         CATALOG.forEach(({ id, dey, dey_utr, dey_vech, color_utr, color_vech}) => {
-          
+          let col_utr;
+          let col_vech;  
             
             
-            
-            
-            
+            if (dey_utr === 0) {
+   col_utr = "#33cc33";
+ 
+} else {
+    col_utr = "#cc3333";
+}
+		
+		
+            if (dey_vech === 0) {
+   col_vech = "#33cc33";
+ 
+} else {
+   col_vech = "#cc3333";
+}            
 
 htmlCatalog += `
 
@@ -33,7 +45,7 @@ htmlCatalog += `
 
 </li>
 <ul class="days">  
-  <li><span class="active"  style = "background: linear-gradient(${color_utr} 50%, ${color_vech} 50%); padding: 1px 10px 1px 10px">${dey}</span></li>
+  <li><span class="active"  style = "background: linear-gradient(${col_utr} 50%, ${col_vech} 50%); padding: 1px 10px 1px 10px">${dey}</span></li>
  
 </ul>
 
