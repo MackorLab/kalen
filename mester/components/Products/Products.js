b283b9ee03bd4cfece9c38ef6616838f1cb18fe5
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-09 13:37:23 +0700

    Update Products.js

--- mester/components/Products/Products.js
+++ mester/components/Products/Products.js
@@ -65,7 +65,7 @@ htmlCatalog += `
 
 <li>
   <div class="dropdown">
-  <a class="dropbtn" style = "background: linear-gradient(to top, ${col_utr} 33%, ${col_vech} 33%, ${col_vech} 67%, ${col_utr} 67%); padding: 1px ${otstup}px 1px ${otstup}px; color: #ffffff;">${dey}</a>
+  <a class="dropbtn" style = "background: linear-gradient(to top, ${col_utr} 34%, ${col_vech} 34%, ${col_vech} 66%, ${col_utr} 66%); padding: 1px ${otstup}px 1px ${otstup}px; color: #ffffff;">${dey}</a>
   <div class="dropdown-content">
     <a href="#">9 - 11 МСК</a>
     <a href="#">13 - 15 МСК</a>
