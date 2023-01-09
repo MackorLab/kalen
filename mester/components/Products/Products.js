de9df2865b612e02c1579f8354a48d1dd1eda335
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-09 12:57:26 +0700

    Update Products.js

--- mester/components/Products/Products.js
+++ mester/components/Products/Products.js
@@ -22,9 +22,9 @@ class Products {
         CATALOG.forEach(({ id, dey, dey_utr, dey_vech, color_utr, color_vech, pop_vid}) => {
           let col_utr;
           let col_vech;  
+          let otstup;  
             
-            
-            if (dey_utr === 0) {
+if (dey_utr === 0) {
    col_utr = "#33cc33";
  
 } else {
@@ -32,12 +32,28 @@ class Products {
 }
 		
 		
-            if (dey_vech === 0) {
+if (dey_vech === 0) {
    col_vech = "#33cc33";
  
 } else {
    col_vech = "#cc3333";
-}            
+}   
+
+		
+		
+		
+		
+if (dey > 9) {
+   otstup = 4;
+ 
+} else {
+   otstup = 10;
+} 		
+		
+		
+		
+		
+		
 
 htmlCatalog += `
 
@@ -49,7 +65,7 @@ htmlCatalog += `
 
 <li>
   <div class="dropdown">
-  <a class="dropbtn" style = "background: linear-gradient(#33cc33 50%, #33cc33 50%); padding: 1px 4px 1px 4px; color: red;">${dey}</a>
+  <a class="dropbtn" style = "background: linear-gradient(#33cc33 50%, #33cc33 50%); padding: 1px ${otstup}px 1px ${otstup}px; color: red;">${dey}</a>
   <div class="dropdown-content">
     <a href="#">9 - 11МСК</a>
     <a href="#">13 - 15МСК</a>
