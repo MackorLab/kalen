2ec1644dd601c6fa84c430cf6ba23328a91645d6
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-09 14:27:22 +0700

    Update Header.js

--- mester/components/Header/Header.js
+++ mester/components/Header/Header.js
@@ -8,9 +8,7 @@ class Header {
            
               
    <div class="hedr_sdvg">  
-     <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
-                    <a class="header-icon"><img src="https://i.ibb.co/xjQM8kr/shoppingcart-to-compra-12829.png" alt="shoppingcart" border="0"></a> 
-                 </div>        
+           
          <div class="month">      
   <ul>
    
