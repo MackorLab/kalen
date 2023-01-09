baefa18ffbcc8f9ae294d8927fb4e9a1d6755de6
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-09 16:42:22 +0700

    Update Shopping.js

--- mester/components/Shopping/Shopping.js
+++ mester/components/Shopping/Shopping.js
@@ -23,7 +23,7 @@ var reloads;
                     htmlCatalog += `
                         <tr>
                             <td class="shopping-element__name">⚡️ ${name}</td>
-                            <td class="shopping-element__price">${datas_hen} ₽</td>
+                            <td class="shopping-element__price">${datas} ₽</td>
                         </tr>
                     `;
                     sumCatalog += price;
@@ -44,7 +44,7 @@ if (sumCatalog > 0) {
           ${htmlCatalog}
           <tr>
               <td class="shopping-element__name">💥 Сумма:</td>
-              <td class="shopping-element__price">${datas_hen} ₽</td>
+              <td class="shopping-element__price">${datas} ₽</td>
           </tr>
       </table>
       <button type="button" id="submits" class="btn btn-outline-warning" onclick="alerted();">Оформить заказ</button>
@@ -62,7 +62,7 @@ ROOT_SHOPPING.innerHTML = html;
                         ${htmlCatalog}
                         <tr>
                             <td class="shopping-element__name">💥 Сумма:</td>
-                            <td class="shopping-element__price">${datas_hen} ₽</td>
+                            <td class="shopping-element__price">${datas} ₽</td>
                         </tr>
                     </table>
                    
