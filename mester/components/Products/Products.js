25a78f1165f6089df934944a90e7c250f5b755dc
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-09 19:03:35 +0700

    Update Products.js

--- mester/components/Products/Products.js
+++ mester/components/Products/Products.js
@@ -53,7 +53,7 @@ if (dey_dey === 0) {
 		
 if (dey_vech === 0) {
 col_vech = "#33cc33";
-} else if (dey_vechy === 1) {
+} else if (dey_vech === 1) {
     col_vech = "#cc3333";
 } else if (dey_vech === 2) {
    col_vech = "#b2d6b2";
