ae1adcb505856d838141235aeb7fce1be6b64695
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-09 16:56:17 +0700

    Update Products.js

--- mester/components/Products/Products.js
+++ mester/components/Products/Products.js
@@ -67,7 +67,7 @@ htmlCatalog += `
 
 
 <li>
-<a onclick="headerPage.handlerOpenShoppingPage();" class="dropbtn" style = "background: linear-gradient(to top, ${col_utr} 34%, ${col_vech} 34%, ${col_vech} 66%, ${col_utr} 66%); padding: 1px ${otstup}px 1px ${otstup}px; color: #ffffff;">${dey}</a>
+<a onclick="openPop_dey_${dey}();" class="dropbtn" style = "background: linear-gradient(to top, ${col_utr} 34%, ${col_vech} 34%, ${col_vech} 66%, ${col_utr} 66%); padding: 1px ${otstup}px 1px ${otstup}px; color: #ffffff;">${dey}</a>
 </li>
 
 
