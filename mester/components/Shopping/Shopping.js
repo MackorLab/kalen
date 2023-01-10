b94f63d37f7ed6e590201f80fc7cbc8eec925651
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-11 03:35:33 +0700

    Update Shopping.js

--- mester/components/Shopping/Shopping.js
+++ mester/components/Shopping/Shopping.js
@@ -209,7 +209,23 @@ if (dey_utr == 0) {
             
   <H3> К сожалению на на эту дату все забронированно! </H3>
   
+  
+  
+  
+  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+  
+  
+  
+  
+  
+  
   </div>
+  
   
   `; 
 }  
