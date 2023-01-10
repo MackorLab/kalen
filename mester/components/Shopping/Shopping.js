ccedf36f3a08e7ffa05c12f543c087e2d44e7cb9
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-10 16:13:47 +0700

    Update Shopping.js

--- mester/components/Shopping/Shopping.js
+++ mester/components/Shopping/Shopping.js
@@ -70,7 +70,7 @@ if (dey_vech == 0) {
 <button class="accordion">Section 2</button>
 <div class="panel">
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-</div>                 
+</div>               
                    
                    
                    
@@ -111,17 +111,16 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
-
 
 
 
