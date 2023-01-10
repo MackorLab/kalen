3b8b62911eea9498ca02cc8c726c5267399ab3c9
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-10 12:39:00 +0700

    Update Shopping.js

--- mester/components/Shopping/Shopping.js
+++ mester/components/Shopping/Shopping.js
@@ -55,6 +55,27 @@ if (dey_vech == 0) {
                         </tr>
                     </table>
                    
+                   
+                   
+                   
+                   
+                   
+                   
+                   
+  <button class="accordion">Section 1</button>
+<div class="panel">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>
+
+<button class="accordion">Section 2</button>
+<div class="panel">
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+</div>                 
+                   
+                   
+                   
+                   
+                   
                 </div>
             `;
     
