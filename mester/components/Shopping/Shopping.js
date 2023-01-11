77a8137a52335d389f79cf57da70bf7871ba7080
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-11 17:41:03 +0700

    Update Shopping.js

--- mester/components/Shopping/Shopping.js
+++ mester/components/Shopping/Shopping.js
@@ -28,7 +28,23 @@ var reloads;
       <p> Имя</p>
       <input id="name" name='name'><br>
         <p> Телефон</p>
-      <input id="phone" name='phone'><br>
+        
+        
+    <div class="form-group">
+    <label for="phone">Телефон: </label>
+    <select id="country" class="form-control">
+      <option value="ru">Россия +7</option>
+      <option value="ua">Украина +380</option>
+      <option value="by">Белоруссия +375</option>
+    </select>
+    <input id="phone" type="text" class="form-control tel" value="">
+  </div>
+        
+        
+        
+        
+        
+    
       
       <br>
   </fieldset>
