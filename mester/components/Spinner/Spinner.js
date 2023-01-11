b4413c17c3dbc1106989b76ba9f0ffa73d00e3d2
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-11 13:25:50 +0700

    Update Spinner.js

--- mester/components/Spinner/Spinner.js
+++ mester/components/Spinner/Spinner.js
@@ -9,7 +9,7 @@ class Spinner {
           
          <div class="box"> 
           <div class="d-flex justify-content-center"> 
-         <img src="https://vk.com/sticker/78155/256.png" alt="Avatar" style="width:100%;">
+         <img src="https://xn--74-6kcaethvbmg4e6b.xn--p1ai/images/uslugi/44.jpg" alt="Avatar" style="width:100%;">
          </div>
          
         <div class="d-flex justify-content-center"> 
