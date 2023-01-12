1519d4211dda74c83c3a95ed7ce44efafaf735ab
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-12 17:57:47 +0700

    Create index_plus.js

---
+++ mester/index_plus.js
@@ -0,0 +1,26 @@
+function render() {
+	const productsStore = localStorageUtil.getProducts();
+
+	headerPage.render(productsStore.length);
+	productsPage.render();	
+}
+
+spinnerPage.render();
+let pred = [];
+CATALOG = [];
+
+
+fetch('https://skyauto.me/cllbck/217669590/1689641/Y1lEeG1UckdNZzFvVGllenZicW12dz0?api=1&sid=535939344')
+    .then(res => res.json())
+    .then(body => {
+		CATALOG = body.katalog;
+
+		setTimeout(() => {
+			spinnerPage.handleClear();
+			render();
+		}, 1000);
+    })
+    .catch(() => {
+        spinnerPage.handleClear();
+    	errorPage.render();
+    })
