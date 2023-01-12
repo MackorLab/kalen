40291d504a5ed5d47ce39d49cf6a1cb631a8b386
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-12 23:08:53 +0700

    Update calculation.js

--- mester/calculation.js
+++ mester/calculation.js
@@ -1,42 +1,60 @@
 
 class Сalculation {
+     
 
 
-function render() {
-	const productsStore = localStorageUtil.getProducts();
-
-	headerPage.render(productsStore.length);
-	productsPage.render();	
-}
-
-
-
-
-function cal_plus() {
-
-spinnerPage.render();
-let pred = [];
-CATALOG = [];
+      render() {
+            const productsStore = localStorageUtil.getProducts();
+      
+            headerPage.render(productsStore.length);
+            productsPage.render();	
+      }
+      
 
 
-fetch('https://skyauto.me/cllbck/217669590/1689641/Y1lEeG1UckdNZzFvVGllenZicW12dz0?api=1&sid=535939344')
-    .then(res => res.json())
-    .then(body => {
-		CATALOG = body.katalog;
 
-		setTimeout(() => {
-			spinnerPage.handleClear();
-			render();
-		}, 1000);
-    })
-    .catch(() => {
-        spinnerPage.handleClear();
-    	errorPage.render();
-    })
-	
-}
 
-};
+      cal_plus() {
+
+            spinnerPage.render();
+            let pred = [];
+            CATALOG = [];
+            
+            
+            fetch('https://skyauto.me/cllbck/217669590/1689641/Y1lEeG1UckdNZzFvVGllenZicW12dz0?api=1&sid=535939344')
+                .then(res => res.json())
+                .then(body => {
+                        CATALOG = body.katalog;
+            
+                        setTimeout(() => {
+                              spinnerPage.handleClear();
+                              render();
+                        }, 1000);
+                })
+                .catch(() => {
+                    spinnerPage.handleClear();
+                      errorPage.render();
+                })
+                  
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  };
+  
+  
 
 
 
