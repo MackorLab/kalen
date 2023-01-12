57aa81867056edb58f8e1c8f93ffb608468e6aea
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-13 03:41:47 +0700

    Update calculation.js

--- mester/calculation.js
+++ mester/calculation.js
@@ -21,7 +21,7 @@ class Calculation {
             CATALOG = [];
             
             
-            fetch('https://skyauto.me/cllbck/217669590/1696073/bFplaitsM0NBdnlPWTRrVkZOaWllUT0?api=1&sid=535939344')
+            fetch('https://skyauto.me/cllbck/217669590/1689641/Y1lEeG1UckdNZzFvVGllenZicW12dz0?api=1&sid=535939344')
                 .then(res => res.json())
                 .then(body => {
                         CATALOG = body.katalog;
