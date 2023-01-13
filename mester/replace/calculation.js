ada7236b93d23dc6c8ef35bc1df5efd200a8875a
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-13 22:35:35 +0700

    Update calculation.js

--- mester/replace/calculation.js
+++ mester/replace/calculation.js
@@ -83,7 +83,37 @@ class Calculation {
      
      
      
-     
+     oplat_uspex() {
+
+            spinnerPage.render();
+            
+            let pred = [];
+           month = [CATALOG[0].month];
+           let month_new;
+           let month_new_otpr;
+           
+            let num = Number(month);
+           console.log(num);
+           month_new = num+1;
+           console.log(month_new);
+           month_new_otpr = String(month_new)
+           console.log(month_new_otpr);
+            fetch('https://skyauto.me/cllbck/' + p + '?api=1&uid=' + vkid + '&vxod=0&month=0&id_dey=0&periud=0',)
+                .then(res => res.json())
+                .then(body => {
+                        CATALOG = body.katalog;
+            
+                        setTimeout(() => {
+                              spinnerPage.handleClear();
+                              render();
+                        }, 1000);
+                })
+                .catch(() => {
+                    spinnerPage.handleClear();
+                      errorPage.render();
+                })
+                  
+            };
      
 
 
