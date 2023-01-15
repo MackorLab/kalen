2a7b22cce7957978a0d19c5a3cb6ef3a249336fb
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-15 17:14:59 +0700

    Update Shopping.js

--- mester/components/Shopping/Shopping.js
+++ mester/components/Shopping/Shopping.js
@@ -457,6 +457,26 @@ if (!validateEmail(emails)){
       
  
      console.log('Не соответствует имя'); 
+} else if (!validateEmail(emails)) {
+    
+    
+       Toastify({
+  text: "Некорректно заполнено поле Email",
+  duration: 3000,
+  newWindow: true,
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: "left", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  style: {
+    background: "linear-gradient(to right, #b00000, #e04343)",
+  },
+  onClick: function(){} // Callback after click
+}).showToast();
+    
+    
+  
+    console.log('Не соответствует телефон');
 } else if (!validatePhone(phone)) {
     
     
