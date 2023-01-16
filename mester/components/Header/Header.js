77ce2ee8e4880ee3a37e38db48a6e82774aa9c66
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-17 01:48:48 +0700

    Update Header.js

--- mester/components/Header/Header.js
+++ mester/components/Header/Header.js
@@ -112,7 +112,9 @@ month_vivod = "Декабрь";
 	
 }
 
-
+.btn_send:hover {
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}
 </style>     
    <div class="hedr_sdvg"> 
 
