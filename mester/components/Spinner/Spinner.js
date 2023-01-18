185814eded4316eeed298a993ccd5a581f5a1608
MackorLab <76862637+MackorLab@users.noreply.github.com>
2023-01-19 03:05:49 +0700

    Update Spinner.js

--- mester/components/Spinner/Spinner.js
+++ mester/components/Spinner/Spinner.js
@@ -4,7 +4,39 @@ class Spinner {
   }
 
   render() {
+    
+    let spin;
+    let proxod = 0;
+    
+    
+if (proxod == 0 ) {
+   spin = "#ededed";
+ proxod = proxod + 1;
+} else {
+  let spiner_c = [NASROYKI[0].spiner_c]
+  spin = spiner_c;
+   otstup = 10;
+} 		
+	
+    
+    
       const html = `
+      
+      
+      <style>
+      
+.loader {
+ 
+  border-top: 6px solid ${spin};
+
+}
+      
+      
+      </style>
+      
+      
+      
+      
         <div class="spinner-container">
           
       
